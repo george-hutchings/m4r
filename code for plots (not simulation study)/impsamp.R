2ccff0c0100a97e22b711699b46acd13ece47ca8
ghicl <72383746+ghicl@users.noreply.github.com>
2022-06-14 01:19:45 +0100

    Add files via upload

--- code for plots (not simulation study)/impsamp.R
+++ code for plots (not simulation study)/impsamp.R
@@ -1,6 +1,5 @@
-
 k = 2
-d = 2
+d = 1
 
 
 sigma = lapply(rep.int(1, k), function(x) diag(x, nrow = d))
@@ -11,11 +10,13 @@ w = w / sum(w)
 ## my method
 n = 10e5 # default value
 N = 200
+
 deltas = c(0.1, 100)
 
 
 my_integral = matrix(NA, N, 2)
 other_integral = my_integral
+num_integral = numeric(2)
 for (j in 1:length(deltas)){
   delta = deltas[j]
   mu = matrix(runif(k * d, min = 0, max = delta), nrow = k)
@@ -26,7 +27,7 @@ samples = rmixn(n = n,mu = mu,sigma = sigma, w = w)
 f = dmixn(X = samples,mu = mu,sigma = sigma, w = w)
 #h = f**2
 integral.val = mean(f) #= mean(h/f)h/f = f
-my_integral[i,j] = integra.val
+my_integral[i,j] = integral.val
 
 
 
@@ -37,6 +38,8 @@ integral.val2 = mean(h/f)
 other_integral[i,j] = integral.val2
 print(i)
 }
+  integrand <- function(x) {dmixn(X = matrix(x),mu = mu,sigma = sigma, w = w)**2}
+  num_integral[j] = integrate(integrand, -10, 110, subdivisions=10000L)$value
   data1 <- data.frame(h = my_integral[,j], B = other_integral[,j])
   boxplot(data1, names=c("f = fitted p_10", "f = t distribution"), cex=0.8)
 }
@@ -49,9 +52,12 @@ par(mfrow=c(1,2))
 data1 <- data.frame(h = my_integral[,1], B = other_integral[,1])
 title = paste('delta=', deltas[1])
 boxplot(data1, main = title, names=c("f = fitted p", "f = t distribution") , cex=0.8)
+abline(h = num_integral[1],col = "Red")
+
 data1 <- data.frame(h = my_integral[,2], B = other_integral[,2])
 title = paste('delta=', deltas[2])
 boxplot(data1, main = title, names=c("f = fitted p", "f = t distribution"), cex=0.8)
+abline(h = num_integral[2],col = "Red")
 dev.off()
 
 
